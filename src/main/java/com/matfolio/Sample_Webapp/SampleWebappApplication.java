397aedb00bc7fe8d0cccab97345abbf509bb66b5
Muftau Abdulrahamon <muftau.abdulrahamon@epassi.com>
2026-05-28 13:23:58 +0300

    Created a new web app project.

---
+++ src/main/java/com/matfolio/Sample_Webapp/SampleWebappApplication.java
@@ -0,0 +1,13 @@
+package com.matfolio.Sample_Webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matfolio/Sample_Webapp/SampleWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matfolio.Sample_Webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
